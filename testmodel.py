b7d32aa7b7255c391354b2776798a3d0edab6de5
amogh7joshi <joshi.amoghn@gmail.com>
2020-11-27 09:33:04 -0500

    Updated model testing

--- testmodel.py
+++ testmodel.py
@@ -10,16 +10,13 @@ from data.load_data import get_data
 
 X_train, X_validation, X_test, y_train, y_validation, y_test = get_data()
 
-model = model_from_json(open("model.json", "r").read())
-model.load_weights(os.path.join(os.path.dirname(__file__), "data", "model", "Model-08-0.5252.hdf5"))
+datadir = os.path.join(os.path.dirname(__file__), "data", "savedmodels")
+model = model_from_json(open(os.path.join(datadir, "model-10-5358.json"), "r").read())
+model.load_weights(os.path.join(datadir, "Model-10-0.5358.hdf5"))
 
 model.compile(optimizer = Adam(),
               loss = categorical_crossentropy,
               metrics = ['accuracy'])
-
-model_json = model.to_json()
-with open("model.json", "w") as savefile:
-   savefile.write(model_json)
 
 loss, acc = model.evaluate(X_test, y_test)
 print(f"Accuracy: {acc * 100:.5f}%")
