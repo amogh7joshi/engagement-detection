c6cc55865adac16997bed6ce2ee2600e4d45e9d8
amogh7joshi <joshi.amoghn@gmail.com>
2021-01-14 17:40:47 -0500

    Add dataset shuffling method, dataset validation decorator

--- data/dataset_ops.py
+++ data/dataset_ops.py
@@ -5,13 +5,42 @@ import os
 import cv2
 import numpy as np
 
+__all__ = ['reduce_dataset', 'shuffle_dataset']
+
+def validate_datasets(func):
+   """Decorator method to validate datasets and keyword arguments."""
+   def inner(*args, **kwargs):
+      for arg_item in args: # Validate dataset arguments.
+         if not isinstance(arg_item, (np.ndarray, list, tuple)):
+            raise TypeError(f"Invalid types provided for arguments, got {type(arg_item)}")
+      for kwarg_item in kwargs: # Validate reduction/shuffle keyword arguments.
+         if kwarg_item == 'reduction':
+            if func.__name__ == 'shuffle_dataset':
+               raise ValueError("The 'reduction' keyword argument applies to the reduce_dataset method,"
+                                "not the shuffle_dataset method. Try again with the right method.")
+            if not isinstance(kwargs['reduction'], (int, float)):
+               raise TypeError(f"Invalid type provided for reduction, should be either int or float. "
+                               f"Got {type(kwargs['reduction'])}")
+         elif kwarg_item == 'shuffle':
+            if func.__name__ == 'shuffle_dataset':
+               raise ValueError("The 'reduction' keyword argument applies to the reduce_dataset method,"
+                                "not the shuffle_dataset method, which is shuffling by default.")
+            if not isinstance(kwargs['shuffle'], bool):
+               raise TypeError(f"Invalid type provided for shuffle, should be either True or False. "
+                               f"Got {type(kwargs['shuffle'])}")
+         else: # If received a non-existing keyword argument.
+            raise ValueError(f"Received invalid keyword argument '{kwarg_item}'.")
+
+      # Execute function.
+      return func(*args, **kwargs)
+   return inner
+
+@validate_datasets
 def reduce_dataset(*args, reduction = None, shuffle = True):
    """Reduce the size of a dataset, only selecting a certain part of it."""
    data_items = []; data_shape = np.array(args[0]).shape[0]
    # Validate items and convert to numpy arrays.
    for item in args:
-      if not isinstance(item, (np.ndarray, list, tuple)):
-         raise TypeError(f"Invalid types provided for arguments, got {type(item)}")
       if isinstance(item, (list, tuple)):
          data_items.append(np.array(item))
       else:
@@ -20,10 +49,11 @@ def reduce_dataset(*args, reduction = None, shuffle = True):
          raise ValueError("All items provided must be from the same dataset, and therefore have the same length.")
 
    # Choose reduction and return, based on randomization.
-   if not isinstance(reduction, int):
-      raise TypeError("You need to provide an integer value for reduction.")
+   if isinstance(reduction, float):
+      reduction = int(reduction * data_shape)
    if reduction > data_items[0].shape[0]:
-      raise ValueError(f"You have provided a length longer than that of the dataset: {reduction} > {data_items[0].shape[0]}.")
+      raise ValueError(f"You have provided a length longer than that of the dataset: "
+                       f"{reduction} > {data_items[0].shape[0]}.")
    if shuffle: # If randomization, then select random items from dataset.
       reduction_list = np.random.choice(data_shape, reduction, replace = False)
       reduced_items = []
@@ -42,5 +72,36 @@ def reduce_dataset(*args, reduction = None, shuffle = True):
    if len(reduced_items) == 1:
       return reduced_items[0]
    return reduced_items
+
+@validate_datasets
+def shuffle_dataset(*args):
+   """Reduce the size of a dataset, only selecting a certain part of it."""
+   data_items = []; data_shape = np.array(args[0]).shape[0]
+   # Validate items and convert to numpy arrays.
+   for item in args:
+      if not isinstance(item, (np.ndarray, list, tuple)):
+         raise TypeError(f"Invalid types provided for arguments, got {type(item)}")
+      if isinstance(item, (list, tuple)):
+         data_items.append(np.array(item))
+      else:
+         data_items.append(item)
+      if np.array(item).shape[0] != data_shape:
+         raise ValueError("All items provided must be from the same dataset, and therefore have the same length.")
+
+   # Shuffle dataset and return.
+   random_list = np.random.choice(data_shape, data_shape, replace = False)
+   shuffled_items = []
+   for item in data_items:
+      current_list  = []
+      for value in random_list:
+         current_list.append(item[value])
+      current_list = np.array(current_list)
+      shuffled_items.append(current_list)
+
+   # Return individual item if only one is provided, otherwise return all.
+   if len(shuffled_items) == 1:
+      return shuffled_items[0]
+   return shuffled_items
+
 
 
