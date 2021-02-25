22d17b452815dbd911c0a5854456cea126c5dbc9
amogh7joshi <joshi.amoghn@gmail.com>
2021-02-25 13:22:17 -0500

    Update utilities with Keras to TFLife, Keras to SavedModel

--- util/converter.py
+++ util/converter.py
@@ -9,7 +9,8 @@ import tensorflow as tf
 def keras_to_tf_lite(keras_model_path, output_model_path):
    """Converts a Keras model to TensorFlow Lite."""
    # Set up the converter.
-   converter = tf.lite.TFLiteConverter.from_keras_model(keras_model_path)
+   converter = tf.lite.TFLiteConverter.from_keras_model(
+      tf.keras.models.load_model(keras_model_path))
 
    # Ensure the provided path exists.
    if not os.path.exists(keras_model_path):
@@ -21,5 +22,17 @@ def keras_to_tf_lite(keras_model_path, output_model_path):
    # Save the model.
    with open(output_model_path, 'wb') as save_file:
       save_file.write(tf_lite_model)
+
+def keras_to_saved_model(keras_model_path, output_model_directory):
+   """Converts a Keras model to a TensorFlow SavedModel."""
+   # Ensure the provided path exists.
+   if not os.path.exists(keras_model_path):
+      raise FileNotFoundError(f"The provided Keras model path {keras_model_path} does not exist.")
+
+   # Load the model.
+   model = tf.keras.models.load_model(keras_model_path)
+
+   # Save the model.
+   model.save(output_model_directory)
 
 
